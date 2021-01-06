b5be506c4805ce208c53df0e68539c56e50404ed
Maeling <maelingmurphy@users.noreply.github.com>
2021-01-06 17:42:12 -0500

    Created variables for the form fields to be used on add.html

--- forms.py
+++ forms.py
@@ -1,15 +1,36 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextField, SelectField, SubmitField # Field Types
-from wtforms.validators import DataRequired, Length # Validators
+from wtforms import validators, TextField, TextAreaField, SelectField, BooleanField, SubmitField # Field Types
+from wtforms.fields.html5 import DateField # Renders datepicker
+from wtforms.validators import DataRequired # Validators
 
 class AddActivity(FlaskForm):
     """Add activity form."""
-    title = StringField(
+    
+    # Activity Title 
+    title = TextField(
         'Title',
         [DataRequired()]
     )
-    description = TextField(
-        'Activity Description',
+
+    # Activity Description
+    description = TextAreaField(
+        'Activity Description'
     )
+
+    # Select subject
     subject = SelectField(u'Subject', choices=[('math', 'Math'), ('read', 'Reading'), ('sci', 'Science')])
+    resources = TextAreaField(
+        'Resources'
+    )
+
+    # Select student(s)
+    students = SelectField(u'Student(s)', choices=[('1', 'Student 1'), ('2', 'Student 2'), ('3', 'All Students')])
+
+    # Activity date selector
+    activity_date = DateField('Activity Date', format='%Y-%m-%d', validators=(validators.DataRequired(),))
+    
+    # Checkbox for marking completed activity
+    status = BooleanField('Completed?')
+
+    # Submit form
     submit = SubmitField('Add Activity')
