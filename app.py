3478504b0504f31de10cdf5af35937c01c807a28
Maeling <maelingmurphy@users.noreply.github.com>
2021-01-05 23:15:46 -0500

    Linked to forms.py, config files

--- app.py
+++ app.py
@@ -1,15 +1,34 @@
-from flask import Flask, render_template, url_for
+from flask import (
+    Flask, 
+    render_template, 
+    url_for,
+    redirect
+)
 
-app = Flask(__name__) # Refers to this current file 
+
+# Import AddActivity class from forms.py
+from forms import AddActivity 
+
+# Flask app creation 
+app = Flask(__name__, instance_relative_config=True) # Refers to this current file 
+app.config.from_object('config')
+app.config.from_pyfile('config.py') # Load configuration variables from an instance folder
 
 # Home Page
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
-@app.route("/add")
+@app.route('/add', methods=('GET', 'POST'))
 def add():
-    return render_template("add.html")
-    
+    form = AddActivity()
+    if form.validate_on_submit():
+        return 'Activity has been added'
+
+    return render_template(
+        'add.html',
+        form=form
+    )
+
 if __name__ == "__app__":
     app.run(debug=True)
