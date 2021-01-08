16d2f66ef20bc668f2dc8ea98907c105d13a121d
Maeling <maelingmurphy@users.noreply.github.com>
2021-01-08 17:37:50 -0500

    Cleaned up formatting

--- app.py
+++ app.py
@@ -5,20 +5,25 @@ from flask import (
     redirect
 )
 
-
 # Import AddActivity class from forms.py
 from forms import AddActivity 
+
 
 # Flask app creation 
 app = Flask(__name__, instance_relative_config=True) # Refers to this current file 
 app.config.from_object('config')
 app.config.from_pyfile('config.py') # Load configuration variables from an instance folder
 
+if __name__ == "__app__":
+    app.run(debug=True)
+
 # Home Page
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
+# Add Activity 
 @app.route('/add', methods=('GET', 'POST'))
 def add():
     form = AddActivity()
@@ -30,5 +35,7 @@ def add():
         form=form
     )
 
-if __name__ == "__app__":
-    app.run(debug=True)
+# Activity Log
+@app.route('/log')
+def log():
+    return render_template('log.html')
