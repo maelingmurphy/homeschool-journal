a6f39386c4c7c9dfcd982d41d94f9280ee62518c
Maeling <maelingmurphy@users.noreply.github.com>
2021-01-10 14:11:20 -0500

    Added control statements and mock objects to display activity info in table

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,15 @@
+from app import app
+
+
 from flask import (
-    Flask, 
     render_template, 
     url_for,
     redirect
 )
-from app import app
+
 
 # Import AddActivity class from forms.py
-from forms import AddActivity 
+#from forms import AddActivity 
 
 
 
@@ -16,29 +18,45 @@ from forms import AddActivity
 # Home Page
 @app.route('/')
 def index():
-    return render_template('index.html')
+    date = 'Sunday - January 10, 2020'
+    return render_template('index.html', date=date )
 
 
 # Add Activity 
-@app.route('/add', methods=('GET', 'POST'))
-def add():
-    form = AddActivity()
-    if form.validate_on_submit():
-        return 'Activity has been added'
+#@app.route('/add', methods=('GET', 'POST'))
+#def add():
+#    form = AddActivity()
+#    if form.validate_on_submit():
+#        return 'Activity has been added'
 
-    return render_template(
-        'add.html',
-        form=form
-    )
+#    return render_template(
+#        'add.html',
+#        form=form
+#    )
 
 # Activity Log
 @app.route('/log')
 def log():
-    form = AddActivity()
-    if form.validate_on_submit():
-        return 'Activity has been added'
+    activities = [
+        {'date': 'January 10, 2020',
+        'title': 'Intro to Multiplication',
+        'subject': 'Math',
+        'student': 'James',
+        'status': 'completed'},
+        {'date': 'January 10, 2020',
+        'title': 'Prisms',
+        'subject': 'Physics',
+        'student': 'James',
+        'status': 'completed'}
+    ]
 
-    return render_template(
-        'log.html',
-        form=form
-    )
+    return render_template('log.html', activities=activities)
+
+#    form = AddActivity()
+#    if form.validate_on_submit():
+#        return 'Activity has been added'
+
+#    return render_template(
+#        'log.html',
+#        form=form
+#    )
