fc45003826951eb7eaf059320e8c77a574ea8338
Maeling <maelingmurphy@users.noreply.github.com>
2021-01-09 19:26:16 -0500

    Added 'Config' class to config.py and 'if else' logic to app.py for choosingwhich Config attributes will be applied

--- app.py
+++ app.py
@@ -5,14 +5,23 @@ from flask import (
     redirect
 )
 
+from config import Config
+
 # Import AddActivity class from forms.py
 from forms import AddActivity 
 
 
 # Flask app creation 
 app = Flask(__name__, instance_relative_config=True) # Refers to this current file 
-app.config.from_object('config')
-app.config.from_pyfile('config.py') # Load configuration variables from an instance folder
+
+#app.config.from_pyfile('config.py') # Load configuration variables from an instance folder
+
+if app.config['ENV'] == 'production':
+    app.config.from_object('config.ProductionConfig')
+else:
+    app.config.from_object('config.DevelopmentConfig')
+
+print(f'ENV is now set to: {app.config["ENV"]}')
 
 if __name__ == "__app__":
     app.run(debug=True)
@@ -38,4 +47,11 @@ def add():
 # Activity Log
 @app.route('/log')
 def log():
-    return render_template('log.html')
+    form = AddActivity()
+    if form.validate_on_submit():
+        return 'Activity has been added'
+
+    return render_template(
+        'log.html',
+        form=form
+    )
--- config.py
+++ config.py
@@ -1,1 +1,17 @@
-DEBUG = True
+# Create the Config class and set default attributes 
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    SECRET_KEY = 'testingRANDOMkey801e982734019G825g7!084560A2398571R%03874023;46E984701298437'
+
+# Config class for running in production
+class ProductionConfig(Config):
+    pass
+
+# Config class for running in development
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+# Config class for testing
+class TestingConfig(Config):
+    TESTING = True 
