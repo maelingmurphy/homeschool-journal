6de9d08910b957ed0f4d8a51bb9cbbfcce253fee
Maeling <maelingmurphy@users.noreply.github.com>
2021-01-11 00:25:03 -0500

    Commented out code for testing

--- app/__init__.py
+++ app/__init__.py
@@ -1,31 +1,41 @@
 from flask import Flask
 from config import Config
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+from app import routes
+
+# CODE BELOW THAT NEEDS TO BE TESTED
+
+#from config import Config
+#from flask_sqlalchemy import SQLAlchemy
+#from flask_migrate import Migrate
+
+#def create_app():
+    # Flask app creation 
+    #app = Flask(__name__, instance_relative_config=True) # Refers to this current file 
+
+    # Set app configuration variables 
+    #if app.config['ENV'] == 'production':
+    #    app.config.from_object('config.ProductionConfig')
+    #else:
+    #    app.config.from_object('config.DevelopmentConfig')
+
+    #from app import models
+
+    # Database instance
+    #db = SQLAlchemy(app) # db object that represents the database 
+
+    # Database migration instance
+    #migrate = Migrate(app, db) # migrate object that represents the migration engine 
+
+    #print(f'ENV is now set to: {app.config["ENV"]}')
+
+    #return app
 
 
-# Flask app creation 
-app = Flask(__name__, instance_relative_config=True) # Refers to this current file 
-
-# Set app configuration variables 
-if app.config['ENV'] == 'production':
-    app.config.from_object('config.ProductionConfig')
-else:
-    app.config.from_object('config.DevelopmentConfig')
-
-print(f'ENV is now set to: {app.config["ENV"]}')
-
-if __name__ == "__app__":
-    app.run(debug=True)
-
-# Database instance
-db = SQLAlchemy(app) # db object that represents the database 
-
-# Database migration instance
-migrate = Migrate(app, db) # migrate object that represents the migration engine 
-
-# Import AddActivity class from forms.py
-#from forms import AddActivity 
-
-
-from app import routes 
+#from app import (
+#    routes, 
+#    models
+#)
