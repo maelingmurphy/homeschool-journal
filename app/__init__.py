29148d4cb26f20d08c375d51ad72aa0994d18893
Maeling <maelingmurphy@users.noreply.github.com>
2021-01-11 22:15:21 -0500

    Setup for Flask-SQLAlchemy integration

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes
+from app import routes, models
 
 # CODE BELOW THAT NEEDS TO BE TESTED
 
--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or '2349-8T72987403984570huower-3248025452Bja;lksfawr25'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'b7dd41440bb53b6ea4fe57800e5d68063fff80d0748cc693'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False 
