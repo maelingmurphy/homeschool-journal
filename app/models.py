15586385ca14973c1fed5198d90001103a3be326
Maeling <maelingmurphy@users.noreply.github.com>
2021-01-11 22:30:53 -0500

    Added student database model

--- app/models.py
+++ app/models.py
@@ -5,6 +5,15 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    students = db.relationship('Student', backref='admin', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    student_name = db.Column(db.String(64), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Student {}>'.format(self.student_name)
