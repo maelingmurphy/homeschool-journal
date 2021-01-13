6d052d3c19617a34fee4a08032fc6144eb87bf81
Maeling <maelingmurphy@users.noreply.github.com>
2021-01-12 19:04:06 -0500

    Enable batch mode to work with SQlite ALTER TABLE limitations

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from flask_migrate import Migrate
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+migrate = Migrate(app, db, render_as_batch=True)
 
 from app import routes, models
 
