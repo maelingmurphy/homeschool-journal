15bcfaaf225602b3132274858d5a0792520b9348
Maeling <maelingmurphy@users.noreply.github.com>
2021-01-13 18:51:34 -0500

    Added login view and functionality with flask-login

--- app/__init__.py
+++ app/__init__.py
@@ -2,19 +2,20 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db, render_as_batch=True)
+login = LoginManager(app)
 
 from app import routes, models
 
 # CODE BELOW THAT NEEDS TO BE TESTED
 
-#from config import Config
-#from flask_sqlalchemy import SQLAlchemy
-#from flask_migrate import Migrate
+
+
 
 #def create_app():
     # Flask app creation 
@@ -26,20 +27,14 @@ from app import routes, models
     #else:
     #    app.config.from_object('config.DevelopmentConfig')
 
-    #from app import models
+   
 
-    # Database instance
-    #db = SQLAlchemy(app) # db object that represents the database 
+   
 
-    # Database migration instance
-    #migrate = Migrate(app, db) # migrate object that represents the migration engine 
+    
 
     #print(f'ENV is now set to: {app.config["ENV"]}')
 
     #return app
 
 
-#from app import (
-#    routes, 
-#    models
-#)
--- app/routes.py
+++ app/routes.py
@@ -7,7 +7,8 @@ from flask import (
 
 from app import app
 from app.forms import LoginForm, AddActivity # Import LoginForm, AddActivity classes from forms.py
-
+from flask_login import current_user, login_user
+from app.models import User
 
 
 # THE ROUTES 
@@ -81,9 +82,14 @@ def attend():
 # Login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect('/index')
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
