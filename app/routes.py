eefe3f104583271e300e8a56089a2aeda3e97b76
Maeling <maelingmurphy@users.noreply.github.com>
2021-01-16 17:40:45 -0500

    Updated route methods, Linked student name form data to database in index view

--- app/routes.py
+++ app/routes.py
@@ -7,9 +7,9 @@ from flask import (
 )
 
 from app import app, db
-from app.forms import LoginForm, AddActivity, RegistrationForm # Import LoginForm, AddActivity classes from forms.py
+from app.forms import LoginForm, AddStudentForm, AddActivityForm, RegistrationForm # Import classes from forms.py
 from flask_login import current_user, login_user, logout_user, login_required
-from app.models import User
+from app.models import User, Student, Activity
 from werkzeug.urls import url_parse
 
 
@@ -17,12 +17,47 @@ from werkzeug.urls import url_parse
 # THE ROUTES 
 
 # Home Page
-@app.route('/')
-@app.route('/index')
+@app.route('/',  methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required 
 def index():
     date = 'Sunday - January 10, 2020'
-    return render_template('index.html', date=date )
+    
+    # Add Student Form Info
+    form = AddStudentForm()
+    if form.validate_on_submit():
+        
+        # Get student number
+        student_number = int(form.user_student.data)
+
+        # Get student names
+        student_names = []
+        for i in range(1, student_number + 1):
+            student_names.append(request.form.get(f"student_name{i}"))
+
+        # Add student names to database (ADD USER ID!!) and hide 'Add Students' button
+        for name in student_names:
+            student = Student(student_name=name, user_id=current_user.id)
+            db.session.add(student)
+            db.session.commit() 
+        
+        # Add student_number to user table 
+        current_user.student_number = student_number
+        db.session.commit()
+
+        for name in student_names:
+            flash('You have successfully added {} as a student!'.format(name), 'info')
+
+        return redirect(url_for('index'))
+       
+        
+        
+        # Test
+        print("User id: ", current_user.id)
+        print("Number of students: ", student_number)
+        print("Student Names", student_names) 
+
+    return render_template('index.html', date=date, form=form)
 
 # Register User
 @app.route('/register', methods=['GET', 'POST'])
@@ -32,31 +67,12 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(username=form.username.data, email=form.email.data)
-        user.set_password(form.password.data)
-
-        # Get student number
-        student_number = int(form.user_student.data)
-
-        # Get student names
-        student_names = []
-        for i in range(1, student_number + 1):
-            student_names.append(request.form.get(f"student_name{i}"))
-
-        # Add student names to database (ADD USER ID!!)
-        for name in student_names:
-            student = Student(student_name=name, user_id="")
-
-        # Test
-        print(student_number)
-        print("Student Names", student_names) 
-        
-        #db.session.add(user)
-        #db.session.commit()
-
-        
+        user.set_password(form.password.data)   
+        db.session.add(user)
+        db.session.commit()  
         username = form.username.data
-        flash('Congratulations {}, you are now a registered user!'.format(username))
-        #return redirect(url_for('login'))
+        flash('Congratulations {}, you are now a registered user!'.format(username), 'info')
+        return redirect(url_for('login'))
     return render_template('register.html', form=form)
 
 
@@ -69,7 +85,7 @@ def add():
     students = ['James', 'Stephanie', 'Mariah']
     subjects = ['Math', 'Reading', 'Science', 'Geography', 'Physical Education', 'Music', 'Foreign Language']
 
-    form = AddActivity()
+    form = AddActivityForm()
     form.subject.choices = subjects
     form.student.choices = students
 
@@ -77,7 +93,7 @@ def add():
         # Add activity info to database 
         
         # Display flash confirmation message
-        flash('"{}" has been successfully added'.format(form.title.data))
+        flash('"{}" has been successfully added'.format(form.title.data), 'info')
         return redirect(url_for('log'))
 
     return render_template('add.html',form=form, students=students)
@@ -110,10 +126,10 @@ def log():
 
     ]
 
-    form = AddActivity()
+    form = AddActivityForm()
     if form.validate_on_submit():
         # Display flash confirmation message
-        flash('"{}" has been successfully added'.format(form.title.data))
+        flash('"{}" has been successfully added'.format(form.title.data), 'info')
 
     return render_template('log.html', activities=activities, form=form)
 
@@ -133,7 +149,7 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
+            flash('Invalid username or password', 'error')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
