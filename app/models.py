de68dd24153457e944a12561d7d1343f79666959
Maeling <maelingmurphy@users.noreply.github.com>
2021-01-18 18:26:51 -0500

    Change subject to activity relationship from many-to-many to one-to-many

--- app/models.py
+++ app/models.py
@@ -44,6 +44,7 @@ class User(UserMixin, db.Model):
 class Activity(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(150), index=True, nullable=False)
+    subject_id = db.Column(db.String(64), index=True, nullable=False)
     description = db.Column(db.Text, index=True)
     resources = db.Column(db.Text, index=True)
     activity_date = db.Column(db.String(64), index=True, nullable=False)
@@ -75,7 +76,7 @@ class Attendance(db.Model):
 class Subject(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     subject_name = db.Column(db.String(64), index=True)
-    activities = db.relationship('Activity', secondary=subject_activity, backref=db.backref('subject', lazy='dynamic'))
+    activities = db.relationship('Activity', backref='subject', lazy='dynamic'))
 
     def __repr__(self):
         return '<Subject>'.format(self.subject_name)
