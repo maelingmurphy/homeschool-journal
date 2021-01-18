4ef684e5a392a1eedb94a8298d9596ddf12b902e
Maeling <maelingmurphy@users.noreply.github.com>
2021-01-18 16:52:14 -0500

    Added naming_convention and imported MetaData

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,23 @@
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
-db = SQLAlchemy(app)
+
+naming_convention = {
+   "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(column_0_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s" 
+}
+
+db = SQLAlchemy(app=app, metadata=MetaData(naming_convention=naming_convention))
 migrate = Migrate(app, db, render_as_batch=True)
 login = LoginManager(app)
 
