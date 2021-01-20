ec72904a549599c19c11630701eceb39de3e3822
Maeling <maelingmurphy@users.noreply.github.com>
2021-01-20 00:08:26 -0500

    Add Subject model to shell session for flask shell

--- journal.py
+++ journal.py
@@ -1,6 +1,6 @@
 from app import app, db
-from app.models import User, Student
+from app.models import User, Student, Subject
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Student': Student}
+    return {'db': db, 'User': User, 'Student': Student, 'Subject': Subject}
