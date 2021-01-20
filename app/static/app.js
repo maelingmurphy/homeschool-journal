4766f831f0eaed5f340eebfd214243195bb5c2cd
Maeling <maelingmurphy@users.noreply.github.com>
2021-01-19 22:04:22 -0500

    Remove script for creating subject input fields

--- app/static/app.js
+++ app/static/app.js
@@ -5,45 +5,6 @@
 const studentSelect = document.querySelector("#user_student"); // Get SelectField <select> element (where user selects number of students)
 const studentContainer = document.querySelector("#student-container"); // Get #student-container div element where additional inputs will be populated if necessary
 
-const subjectSelect = document.querySelector("#user_subject");
-const subjectContainer = document.querySelector("#subject-container")
-
-// Test function
-function displayInputFields() {
-
-    // Number of subjects selected
-    var subjectNum = subjectSelect.value;
-    
-    // Number of subject name input fields at time of selection
-    var fieldNum = subjectContainer.childElementCount;
-
-    // If subjectNum > fieldNum, add subjectname input fields
-    if (subjectNum > fieldNum) {
-        var addFieldNum = subjectNum - fieldNum;
-        for (i = 0; i < addFieldNum; i++) {
-            var input = document.createElement("input");
-
-            input.type = "text";
-            input.name = `subject_name${i+2}`;
-            input.className = "form-control";
-            input.id = `subject_name${i+2}`;
-            input.placeholder = "Enter subject name here"
-            subjectContainer.appendChild(input);
-        }
-    }
-    // If subjectNum < fieldNum, remove subject name input fields
-    
-    if (fieldNum > subjectNum) {
-        var removeFieldNum = fieldNum - subjectNum;
-        for (i = 0; i < removeFieldNum; i++) {
-            subjectContainer.removeChild(subjectContainer.lastElementChild);
-        }
-    }   
-}
-
-// Add event listener to subject <select> element
-subjectSelect.addEventListener('change', displayInputFields, false)
-
 // Add event listener to student <select> element
 studentSelect.addEventListener('change', (event) => {
 
