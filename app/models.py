0757204f42bfd5727e9a03e0d1c87406cb90c5d4
Maeling <maelingmurphy@users.noreply.github.com>
2021-01-22 15:09:40 -0500

    Add user_id to Subject model, modify User backref to Subject :

--- app/models.py
+++ app/models.py
@@ -18,7 +18,7 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128))
     activities = db.relationship('Activity', backref='admin', lazy='dynamic')
     students = db.relationship('Student', backref='admin', lazy='dynamic')
-    subjects = db.relationship('Subject', backref='admin', lazy='dynamic'))
+    subjects = db.relationship('Subject', backref='admin', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -69,7 +69,7 @@ class Attendance(db.Model):
 
 class Subject(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), index=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), index=True)
     subject_name = db.Column(db.String(64), index=True)
     activities = db.relationship('Activity', backref='subject', lazy='dynamic')
 
