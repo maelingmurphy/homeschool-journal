a57ef40f0c186a1a25cd2c01b04b3207d4ba09f7
Maeling <maelingmurphy@users.noreply.github.com>
2021-01-22 14:38:49 -0500

    Change User db.relationship with Subject to one-to-many

--- app/routes.py
+++ app/routes.py
@@ -131,7 +131,7 @@ def add():
         activity_notes = form.notes.data
         activity_status = form.status.data
 
-        # FIX: Add activity info to database (student_id, subject_id)
+        # Add activity info to database
         activity = Activity(
             title=activity_title, user_id=current_user.id, student_id=student.id, subject_id=subject.id, 
             description=activity_description, resources=activity_resources,
