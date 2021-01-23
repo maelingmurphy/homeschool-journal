e44626cf5e9a5799fc943582068b40cf67dbccc2
Maeling <maelingmurphy@users.noreply.github.com>
2021-01-23 15:59:51 -0500

    Change date format to %Y-%m-%d'

--- app/forms.py
+++ app/forms.py
@@ -69,6 +69,6 @@ class AddActivityForm(FlaskForm):
 
 class AddAttendanceForm(FlaskForm):
     """Add attendance form"""
-    attendance_date = DateField('Date', format='%m-%d-%Y')
+    attendance_date = DateField('Date', format='%Y-%m-%d')
     student = SelectField(u'Student', choices=[])
     submit = SubmitField('Update Attendance')
