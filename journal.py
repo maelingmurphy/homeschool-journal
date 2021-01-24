a3c9d626a00674215bd9bdb523975fc4b33c4e79
Maeling <maelingmurphy@users.noreply.github.com>
2021-01-23 20:47:31 -0500

    Import Attendance model

--- journal.py
+++ journal.py
@@ -1,5 +1,5 @@
 from app import app, db
-from app.models import User, Student, Subject
+from app.models import User, Student, Subject, Attendance
 
 @app.shell_context_processor
 def make_shell_context():
