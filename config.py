11be31fe83031e40f91a3aaa13777e3b3cc4c522
Maeling <maelingmurphy@users.noreply.github.com>
2021-07-12 12:33:44 -0400

    Add configuration variable to log to stdout

--- config.py
+++ config.py
@@ -9,3 +9,4 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False 
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
