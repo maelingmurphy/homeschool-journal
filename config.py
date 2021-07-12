b2f501319f4b2bca60230576390cc8fe05a595fc
Maeling <maelingmurphy@users.noreply.github.com>
2021-07-12 10:19:01 -0400

    Import .env file before Config class is created

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import os
+from dotenv import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'b7dd41440bb53b6ea4fe57800e5d68063fff80d0748cc693'
