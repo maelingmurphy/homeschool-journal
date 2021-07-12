552a93e6e208a04c511f8c5afe55437e39ff8d5c
Maeling <maelingmurphy@users.noreply.github.com>
2021-07-12 10:58:35 -0400

    Remove hard-coded secret key and rely on .env variable instead

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'b7dd41440bb53b6ea4fe57800e5d68063fff80d0748cc693'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False 
