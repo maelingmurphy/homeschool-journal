76b978498ba03affa38d00faa889b0e8dd4e8739
Maeling <maelingmurphy@users.noreply.github.com>
2021-07-14 17:00:19 -0400

    Set environment variables for postgres db credentials

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
     # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
     #     'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        f"postgresql://{os.environ.get('db_username')}:{os.environ.get('db_password')}@localhost:5432/{os.environ.get('db_name')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False 
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
